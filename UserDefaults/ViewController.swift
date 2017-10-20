bdf42af21185bfa2878423da35d3ff527d70823e
Jorge Antonio <jorge_mr@ingenieria.unam.mx>
2017-10-20 16:08:39 -0500

    Saves an array of emojis-description

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -10,16 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emojiTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var previousButton: UIButton!
+    var index = 0
+    var myEmojis : [[String]]!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        emojiLabel.text = ""
+        descriptionLabel.text = ""
+        nextButton.alpha = 0.0
+        previousButton.alpha = 0.0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func saveInfo(_ sender: UIButton) {
+        if emojiTextField.text != "" && descriptionTextField.text != ""{
+            let newEmoji = [emojiTextField.text!,descriptionTextField.text!]
+            if var emojis = UserDefaults.standard.object(forKey: "emoji") as? [[String]]{
+                emojis.append(newEmoji)
+                UserDefaults.standard.set(emojis, forKey: "emoji")
+                print("emoji añadido correctamente")
+            }else{
+                UserDefaults.standard.set([newEmoji], forKey: "emoji")
+                print("emoji guardado correctamente")
+            }
+        }else {
+            print("ingresa todos los campos")
+        }
+    }
+    
+    @IBAction func getInfo(_ sender: UIButton) {
+        if let emoji = UserDefaults.standard.object(forKey: "emoji") as? [[String]]{
+            index = 0
+            myEmojis = emoji
+            emojiLabel.text = myEmojis[index][0]
+            descriptionLabel.text = myEmojis[index][1]
+            nextButton.alpha = 1.0
+            previousButton.alpha = 1.0
+        }
+    }
+    
+    @IBAction func nextEmoji(_ sender: UIButton) {
+        if index < myEmojis.count - 1 {
+            index += 1
+            emojiLabel.text = myEmojis[index][0]
+            descriptionLabel.text = myEmojis[index][1]
+        }
+    }
+    
+    @IBAction func previousEmoji(_ sender: UIButton) {
+        if index >= 1 {
+            index -= 1
+            emojiLabel.text = myEmojis[index][0]
+            descriptionLabel.text = myEmojis[index][1]
+        }
+    }
+    
 }
 
