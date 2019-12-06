0f555ae0b4fbf96c273c90c01fe919b906ed61d1
soffph <soff.dg18@gmail.com>
2019-12-05 21:02:27 -0600

    Squashed commit
    Add Changes file
    Add class Changes
    Add init to Changes class

---
+++ UserDefaults/Changes.swift
@@ -0,0 +1,15 @@
+//
+//  Changes.swift
+//  UserDefaults
+//
+//  Created by sofia delgado gonzalez on 05/12/19.
+//  Copyright © 2019 InformaticaBiomedica. All rights reserved.
+//
+
+import Foundation
+
+class Changes{
+	init() {
+		
+	}
+}
